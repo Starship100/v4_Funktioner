a2ccc54b26b95fab1218fdf4e6f3b0a900762887
Starship100 <jimmy.widlund@outlook.com>
2026-02-07 16:11:43 +0100

    v4_Vad skrivs ut

---
+++ main.py
@@ -0,0 +1,19 @@
+
+# Läsa och förstå kod
+
+# 1a.
+# Tror ingenting skrivs ut.
+def foo(t):
+    print("test")
+
+foo("hej")
+
+# 2a
+# = 15 (Missade att det var 'print' !!!)
+def fun1(x, y):
+    return x * y
+
+print(fun1(3, 5))
+
+#print(3, 5)
+# fun1(3,5)

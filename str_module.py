78b27cec28df5053562840581d86fe2feb9e3b88
Starship100 <jimmy.widlund@outlook.com>
2026-02-09 00:16:58 +0100

    v4_str_module

--- str_module.py
+++ str_module.py
@@ -1,4 +1,4 @@
 
-def string_funktion(name):
+def string_funktion(name, count):
         print(f"{name} jobbar som fasen för att komma ikapp!")
         # print(name + " jobbar som fasen för att komma ikapp!")
